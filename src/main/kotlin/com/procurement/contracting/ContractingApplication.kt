854a909fdacab33524ab568274f1d26b08caedc5
Oleh Herashchenko <joghua@gmail.com>
2018-09-21 16:39:08 +0300

    swarm code refactor

--- src/main/kotlin/com/procurement/contracting/ContractingApplication.kt
+++ src/main/kotlin/com/procurement/contracting/ContractingApplication.kt
@@ -3,10 +3,8 @@ package com.procurement.contracting
 import com.procurement.contracting.config.ApplicationConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication(scanBasePackageClasses = [ApplicationConfig::class])
-@EnableEurekaClient
 class ContractingApplication
 
 fun main(args: Array<String>) {
