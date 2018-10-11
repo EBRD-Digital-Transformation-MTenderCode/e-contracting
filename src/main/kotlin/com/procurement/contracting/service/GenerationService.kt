4963dcf42fa704847dd96b808d4b4037d6a9c70a
andreykolchev <andreykolchev@gmail.com>
2018-10-11 16:47:28 +0300

    refactor

--- src/main/kotlin/com/procurement/contracting/service/GenerationService.kt
+++ src/main/kotlin/com/procurement/contracting/service/GenerationService.kt
@@ -5,29 +5,18 @@ import com.procurement.contracting.utils.milliNowUTC
 import org.springframework.stereotype.Service
 import java.util.*
 
-interface GenerationService {
-
-
-    fun generateRandomUUID(): UUID
-
-    fun generateTimeBasedUUID(): UUID
-
-    fun newOcId(cpId: String, stage: String): String
-
-}
-
 @Service
-class GenerationServiceImpl : GenerationService {
+class GenerationService {
 
-    override fun generateRandomUUID(): UUID {
+    fun generateRandomUUID(): UUID {
         return UUIDs.random()
     }
 
-    override fun generateTimeBasedUUID(): UUID {
+    fun generateTimeBasedUUID(): UUID {
         return UUIDs.timeBased()
     }
 
-    override fun newOcId(cpId: String, stage: String): String {
+    fun newOcId(cpId: String, stage: String): String {
         return cpId + "-" + stage.toUpperCase() + "-" + (milliNowUTC() + Random().nextInt())
     }
 }
