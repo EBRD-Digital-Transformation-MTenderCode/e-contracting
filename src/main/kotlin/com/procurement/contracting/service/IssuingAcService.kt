79a17198ccebaab5a3902a1963150b620d820c92
andreykolchev <andreykolchev@gmail.com>
2018-11-26 21:51:27 +0200

    update contract

--- src/main/kotlin/com/procurement/contracting/service/IssuingAcService.kt
+++ src/main/kotlin/com/procurement/contracting/service/IssuingAcService.kt
@@ -30,8 +30,8 @@ class IssuingAcService(private val acDao: AcDao) {
         if (entity.token.toString() != token) throw ErrorException(INVALID_TOKEN)
         val contractProcess = toObject(ContractProcess::class.java, entity.jsonData)
 
-//        if (contractProcess.contract.status != ContractStatus.PENDING) throw ErrorException(CONTRACT_STATUS)
-//        if (contractProcess.contract.statusDetails != ContractStatusDetails.CONTRACT_PREPARATION) throw ErrorException(CONTRACT_STATUS_DETAILS)
+        if (contractProcess.contract.status != ContractStatus.PENDING) throw ErrorException(CONTRACT_STATUS)
+        if (contractProcess.contract.statusDetails != ContractStatusDetails.CONTRACT_PREPARATION) throw ErrorException(CONTRACT_STATUS_DETAILS)
 
         val relatedItemIds = contractProcess.planning!!.budget.budgetAllocation.asSequence().map { it.relatedItem }.toSet()
         val awardItemIds = contractProcess.award.items.asSequence().map { it.id }.toSet()
