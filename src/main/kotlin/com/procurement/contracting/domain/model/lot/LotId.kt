08fce0db37fa9a6cfbb435c2a3758e53b60fb277
katherine <kate.shaidurova@gmail.com>
2020-04-14 18:08:01 +0300

    ES-2913 Added tryLotId method to LotId

--- src/main/kotlin/com/procurement/contracting/domain/model/lot/LotId.kt
+++ src/main/kotlin/com/procurement/contracting/domain/model/lot/LotId.kt
@@ -1,5 +1,11 @@
 package com.procurement.contracting.domain.model.lot
 
+import com.procurement.contracting.domain.functional.Result
+import com.procurement.contracting.domain.util.extension.tryUUID
+import com.procurement.contracting.infrastructure.fail.Fail
 import java.util.*
 
 typealias LotId = UUID
+
+fun String.tryLotId(): Result<LotId, Fail.Incident.Transform.Parsing> =
+    this.tryUUID()
