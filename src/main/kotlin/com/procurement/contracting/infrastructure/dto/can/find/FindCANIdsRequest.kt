162a31d820e2d4677511b87caba73b08b7f11dcb
katherine <kate.shaidurova@gmail.com>
2020-04-14 18:08:35 +0300

    ES-2913 Added annotations to FindCANIdsRequest

--- src/main/kotlin/com/procurement/contracting/infrastructure/dto/can/find/FindCANIdsRequest.kt
+++ src/main/kotlin/com/procurement/contracting/infrastructure/dto/can/find/FindCANIdsRequest.kt
@@ -1,16 +1,20 @@
 package com.procurement.contracting.infrastructure.dto.can.find
 
-
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class FindCANIdsRequest(
     @param:JsonProperty("cpid") @field:JsonProperty("cpid") val cpid: String,
     @param:JsonProperty("ocid") @field:JsonProperty("ocid") val ocid: String,
-    @param:JsonProperty("states") @field:JsonProperty("states") val states: List<State>,
-    @param:JsonProperty("lotIds") @field:JsonProperty("lotIds") val lotIds: List<String>
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @param:JsonProperty("states") @field:JsonProperty("states") val states: List<State>?,
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    @param:JsonProperty("lotIds") @field:JsonProperty("lotIds") val lotIds: List<String>?
 ) {
     data class State(
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         @param:JsonProperty("status") @field:JsonProperty("status") val status: String?,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         @param:JsonProperty("statusDetails") @field:JsonProperty("statusDetails") val statusDetails: String?
     )
 }
