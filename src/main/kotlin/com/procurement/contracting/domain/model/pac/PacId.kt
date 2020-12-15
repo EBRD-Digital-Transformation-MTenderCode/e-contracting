5d7d94c48e5e59c5862aa1a494d12692a4c60751
katherine <kate.shaidurova@gmail.com>
2020-12-15 15:25:11 +0200

    ES-5075 Added Pac id, status and statusDetails

---
+++ src/main/kotlin/com/procurement/contracting/domain/model/pac/PacId.kt
@@ -0,0 +1,29 @@
+package com.procurement.contracting.domain.model.pac
+
+import com.procurement.contracting.extension.UUID_PATTERN
+import com.procurement.contracting.extension.isUUID
+import java.util.*
+
+class PacId private constructor(val underlying: String) {
+
+    companion object {
+        const val pattern = UUID_PATTERN
+        fun validate(text: String): Boolean = text.isUUID
+        fun orNull(text: String): PacId? =
+            if (validate(text)) PacId(UUID.fromString(text).toString()) else null
+
+        fun generate() = PacId(UUID.randomUUID().toString())
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (this !== other)
+            other is PacId
+                && this.underlying == other.underlying
+        else
+            true
+    }
+
+    override fun hashCode(): Int = underlying.hashCode()
+
+    override fun toString(): String = underlying
+}
---
+++ src/main/kotlin/com/procurement/contracting/domain/model/pac/PacStatusDetails.kt
@@ -0,0 +1,20 @@
+package com.procurement.contracting.domain.model.pac
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+import com.procurement.contracting.domain.model.EnumElementProvider
+
+enum class PacStatusDetails(@JsonValue override val key: String) : EnumElementProvider.Key {
+
+    CONCLUDED("concluded"),
+    ALL_REJECTED("allRejected")
+    ;
+
+    override fun toString(): String = key
+
+    companion object : EnumElementProvider<PacStatusDetails>(info = info()) {
+        @JvmStatic
+        @JsonCreator
+        fun creator(name: String) = PacStatusDetails.orThrow(name)
+    }
+}
