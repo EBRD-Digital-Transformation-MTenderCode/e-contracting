c4e5f0037756f216814182ade04a2855c62b963e
katherine <kate.shaidurova@gmail.com>
2020-12-16 13:20:31 +0200

    ES-5075 Removed RequirementValue and de/serializer

--- src/main/kotlin/com/procurement/contracting/infrastructure/bind/criteria/RequirementValueException.kt
+++
@@ -1,4 +0,0 @@
-package com.procurement.contracting.infrastructure.bind.criteria
-
-class RequirementValueException(requirementValue: String, description: String = "") :
-    RuntimeException("Incorrect value in requirement: '$requirementValue'. $description")
