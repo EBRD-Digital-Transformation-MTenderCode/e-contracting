86f7ceb0c0d066a376f8fd667d069377c1ca0523
katherine <kate.shaidurova@gmail.com>
2021-05-18 18:36:21 +0300

    ES-6090 Added and registered MinReceivedConfResponsesRuleDeserializer

---
+++ src/main/kotlin/com/procurement/contracting/infrastructure/bind/rule/MinReceivedConfResponsesRuleDeserializer.kt
@@ -0,0 +1,31 @@
+package com.procurement.contracting.infrastructure.bind.rule
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.node.JsonNodeType
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.procurement.contracting.application.service.rule.model.MinReceivedConfResponsesRule
+import com.procurement.contracting.exception.ErrorException
+import com.procurement.contracting.exception.ErrorType
+import java.io.IOException
+
+class MinReceivedConfResponsesRuleDeserializer : JsonDeserializer<MinReceivedConfResponsesRule>() {
+
+    @Throws(IOException::class)
+    override fun deserialize(
+        jsonParser: JsonParser,
+        deserializationContext: DeserializationContext
+    ): MinReceivedConfResponsesRule {
+        val quantityNode = jsonParser.readValueAs(ObjectNode::class.java)
+        val quantity = quantityNode.get("quantity")
+        return when (quantity.nodeType) {
+            JsonNodeType.STRING -> MinReceivedConfResponsesRule.String.QuantityValue
+                .creator(quantity.asText())
+                .let { MinReceivedConfResponsesRule.String(it) }
+            JsonNodeType.NUMBER -> MinReceivedConfResponsesRule.Number(jsonParser.intValue)
+            else -> throw ErrorException(ErrorType.JSON_TYPE, jsonParser.currentName)
+        }
+    }
+}
+
