5a6e43ac1d35d4b2cda324108cc05b4ecfb388e2
katherine <kate.shaidurova@gmail.com>
2021-05-19 19:03:48 +0300

    ES-6117 Made Contract id String

--- src/main/kotlin/com/procurement/contracting/application/service/model/CheckContractStateParams.kt
+++ src/main/kotlin/com/procurement/contracting/application/service/model/CheckContractStateParams.kt
@@ -2,7 +2,6 @@ package com.procurement.contracting.application.service.model
 
 import com.procurement.contracting.domain.model.OperationType
 import com.procurement.contracting.domain.model.ProcurementMethodDetails
-import com.procurement.contracting.domain.model.fc.id.FrameworkContractId
 import com.procurement.contracting.domain.model.parseCpid
 import com.procurement.contracting.domain.model.parseEnum
 import com.procurement.contracting.domain.model.parseOcid
@@ -10,7 +9,6 @@ import com.procurement.contracting.domain.model.process.Cpid
 import com.procurement.contracting.domain.model.process.Ocid
 import com.procurement.contracting.infrastructure.fail.error.DataErrors
 import com.procurement.contracting.lib.functional.Result
-import com.procurement.contracting.lib.functional.asFailure
 import com.procurement.contracting.lib.functional.asSuccess
 
 class CheckContractStateParams private constructor(
@@ -101,17 +99,6 @@ class CheckContractStateParams private constructor(
     }
 
     data class Contract(
-        val id: FrameworkContractId
-    ) {
-        companion object {
-            fun tryCreate(id: String): Result<Contract, DataErrors> {
-                val contractId = FrameworkContractId.orNull(id)
-                    ?: return DataErrors.Validation.DataMismatchToPattern(
-                        name = "id", pattern = FrameworkContractId.pattern, actualValue = id
-                    ).asFailure()
-
-                return Contract(contractId).asSuccess()
-            }
-        }
-    }
+        val id: String
+    )
 }
