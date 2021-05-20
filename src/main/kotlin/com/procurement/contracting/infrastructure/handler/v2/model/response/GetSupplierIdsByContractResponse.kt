e6b0a65e5de7adc26c7289605948cf78c86f287a
katherine <kate.shaidurova@gmail.com>
2021-05-20 12:43:51 +0300

    ES-6109 Changed contract id type to PacId

--- src/main/kotlin/com/procurement/contracting/infrastructure/handler/v2/model/response/GetSupplierIdsByContractResponse.kt
+++ src/main/kotlin/com/procurement/contracting/infrastructure/handler/v2/model/response/GetSupplierIdsByContractResponse.kt
@@ -2,12 +2,13 @@ package com.procurement.contracting.infrastructure.handler.v2.model.response
 
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.contracting.domain.model.pac.PacId
 
 data class GetSupplierIdsByContractResponse(
     @param:JsonProperty("contracts") @field:JsonProperty("contracts") val contracts: List<Contract>
 ) {
     data class Contract(
-        @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
+        @param:JsonProperty("id") @field:JsonProperty("id") val id: PacId,
         @param:JsonProperty("suppliers") @field:JsonProperty("suppliers") val suppliers: List<Supplier>
     ) {
         data class Supplier(
