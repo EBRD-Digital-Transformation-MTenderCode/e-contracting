57dbe30413e1ee355e75c366562540d08decf9f2
katherine <kate.shaidurova@gmail.com>
2021-06-08 13:54:43 +0300

    ES-6280 Fixed return types in response

--- src/main/kotlin/com/procurement/contracting/infrastructure/handler/v2/model/response/GetCanByIdsResponse.kt
+++ src/main/kotlin/com/procurement/contracting/infrastructure/handler/v2/model/response/GetCanByIdsResponse.kt
@@ -3,32 +3,39 @@ package com.procurement.contracting.infrastructure.handler.v2.model.response
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.contracting.domain.model.award.AwardId
+import com.procurement.contracting.domain.model.can.CANId
+import com.procurement.contracting.domain.model.can.status.CANStatus
+import com.procurement.contracting.domain.model.can.status.CANStatusDetails
+import com.procurement.contracting.domain.model.document.type.DocumentTypeContract
+import com.procurement.contracting.domain.model.lot.LotId
+import java.time.LocalDateTime
 
 data class GetCanByIdsResponse(
     @param:JsonProperty("contracts") @field:JsonProperty("contracts") val contracts: List<Contract>
 ) {
     data class Contract(
-        @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
-        @param:JsonProperty("status") @field:JsonProperty("status") val status: String,
-        @param:JsonProperty("statusDetails") @field:JsonProperty("statusDetails") val statusDetails: String,
-        @param:JsonProperty("awardId") @field:JsonProperty("awardId") val awardId: String,
-        @param:JsonProperty("lotId") @field:JsonProperty("lotId") val lotId: String,
+        @param:JsonProperty("id") @field:JsonProperty("id") val id: CANId,
+        @param:JsonProperty("status") @field:JsonProperty("status") val status: CANStatus,
+        @param:JsonProperty("statusDetails") @field:JsonProperty("statusDetails") val statusDetails: CANStatusDetails,
+        @param:JsonProperty("awardId") @field:JsonProperty("awardId") val awardId: AwardId,
+        @param:JsonProperty("lotId") @field:JsonProperty("lotId") val lotId: LotId,
 
         @JsonInclude(JsonInclude.Include.NON_EMPTY)
         @param:JsonProperty("documents") @field:JsonProperty("documents") val documents: List<Document>?,
 
-        @param:JsonProperty("date") @field:JsonProperty("date") val date: String
+        @param:JsonProperty("date") @field:JsonProperty("date") val date: LocalDateTime
     ) {
         data class Document(
             @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
-            @param:JsonProperty("documentType") @field:JsonProperty("documentType") val documentType: String,
+            @param:JsonProperty("documentType") @field:JsonProperty("documentType") val documentType: DocumentTypeContract,
             @param:JsonProperty("title") @field:JsonProperty("title") val title: String,
 
             @JsonInclude(JsonInclude.Include.NON_NULL)
             @param:JsonProperty("description") @field:JsonProperty("description") val description: String?,
 
             @JsonInclude(JsonInclude.Include.NON_EMPTY)
-            @param:JsonProperty("relatedLots") @field:JsonProperty("relatedLots") val relatedLots: List<String>?
+            @param:JsonProperty("relatedLots") @field:JsonProperty("relatedLots") val relatedLots: List<LotId>?
         )
     }
 }
