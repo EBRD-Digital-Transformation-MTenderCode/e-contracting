9a3bb5c94862bb5999b61380cdd09239adbfcb05
katherine <kate.shaidurova@gmail.com>
2021-06-17 13:07:06 +0300

    ES-6289 Added session bean for new keyspace

--- src/main/kotlin/com/procurement/contracting/infrastructure/configuration/DaoConfiguration.kt
+++ src/main/kotlin/com/procurement/contracting/infrastructure/configuration/DaoConfiguration.kt
@@ -3,6 +3,7 @@ package com.procurement.contracting.infrastructure.configuration
 import com.datastax.driver.core.Cluster
 import com.datastax.driver.core.PlainTextAuthProvider
 import com.datastax.driver.core.Session
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
@@ -17,17 +18,22 @@ import org.springframework.context.annotation.Configuration
 )
 class DaoConfiguration constructor(private val cassandraProperties: CassandraProperties) {
 
-    internal val cluster: Cluster
-        get() = Cluster.builder()
-            .addContactPoints(*cassandraProperties.getContactPoints())
-            .withoutJMXReporting()
-            .withAuthProvider(PlainTextAuthProvider(cassandraProperties.username, cassandraProperties.password))
-            .build()
+    internal val cluster: Cluster = Cluster.builder()
+        .addContactPoints(*cassandraProperties.getContactPoints())
+        .withoutJMXReporting()
+        .withAuthProvider(PlainTextAuthProvider(cassandraProperties.username, cassandraProperties.password))
+        .build()
+        .init()
 
     @Bean
-    fun session(): Session {
-        val cluster = cluster
-        cluster.init()
+    @Qualifier("ocds")
+    fun ocdsSession(): Session {
+        return cluster.connect(cassandraProperties.oldKeyspaceName)
+    }
+
+    @Bean
+    @Qualifier("contracting")
+    fun contractingSession(): Session {
         return cluster.connect(cassandraProperties.keyspaceName)
     }
 }
